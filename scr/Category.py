dd1bc508c46dbda2282dff131afae419636d559f
Asya <sillestyna@gmail.com>
2024-02-09 23:14:04 +0500

    mistakes's done

--- scr/Category.py
+++ scr/Category.py
@@ -5,12 +5,15 @@ class Category:
     description: str
     goods: list
 
+    count = 0
+    unique = 0
+
     def __init__(self, title, description, goods):
         self.title = title
         self.description = description
         self.goods = goods
-        self.count = len(self.goods)
-        self.unique = len(set(self.goods))
 
-    # def display(self):
+        Category.count += 1
+        Category.unique += len(set(self.goods))
+
 
