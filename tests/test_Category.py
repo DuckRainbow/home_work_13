3e4d2be34f47452bbd1db6226b0522186a395e4d
Asya <sillestyna@gmail.com>
2024-02-12 22:55:03 +0500

    13.2 i hope correct homework

--- tests/test_Category.py
+++ tests/test_Category.py
@@ -9,7 +9,6 @@ class TestCategory(unittest.TestCase):
     def test_init(self):
         self.assertEqual(some_category.title, 'home_apl')
         self.assertEqual(some_category.description, 'some goods for home')
-        self.assertEqual(some_category.goods, ['soap', 'spoon', 'knife', 'spoon', 'brush'])
 
     def test_count(self):
         self.assertEqual(some_category.count, 1)
