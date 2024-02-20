c1d61d0f9dbd5bb56e98e26f80621b371bed7e13
Asya <sillestyna@gmail.com>
2024-02-20 17:41:13 +0500

    hw 15.1 task 1 w/o checking

---
+++ scr/classes_daughter.py
@@ -0,0 +1,17 @@
+from scr.Product import Product
+
+
+class Smartphone(Product):
+    def __init__(self, title, description, _price, quantity, performance, model, memory, color):
+        super().__init__(title, description, _price, quantity)
+        self.performance = performance
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+class LawnGrass(Product):
+    def __init__(self, title, description, _price, quantity, country, growing_time, color):
+        super().__init__(title, description, _price, quantity)
+        self.country = country
+        self.growing_time = growing_time
+        self.color = color
